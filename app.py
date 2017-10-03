491ba0ca603e45ccb3ac30b7ed6f2a41b0babc29
mattg317 <mattg317@gmail.com>
2017-10-03 16:16:05 -0400

    added env

--- app.py
+++ app.py
@@ -2,14 +2,14 @@
 import tweepy
 import time
 import json
-#from keys import key as keys
+import os
 
 
 # Twitter API Keys
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+consumer_key = os.environ.get('consumer_key')
+consumer_secret = os.environ.get('consumer_secret')
+access_token = os.environ.get('access_token')
+access_token_secret = os.environ.get('access_token_secret')
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
