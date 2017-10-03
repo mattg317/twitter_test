acc0b95f84e7ad81d9f4fcb1760fc3decf9cb3a5
mattg317 <mattg317@gmail.com>
2017-10-03 15:54:16 -0400

    working bot

---
+++ app.py
@@ -0,0 +1,31 @@
+# Dependencies
+import tweepy
+import time
+import json
+from keys import key as keys
+
+
+# Twitter API Keys
+consumer_key = keys["consumer"]
+consumer_secret = keys["consumer_secret"]
+access_token = keys["access_token"]
+access_token_secret = keys["access_secret"]
+
+# Setup Tweepy API Authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+counter = 0
+
+
+def tweet_out(count):
+
+    api.update_status("This is a tweet at " + str(count))
+    print(f"{count} Status updated")
+
+
+while(True):
+    tweet_out(counter)
+    counter += 1
+    time.sleep(5)
